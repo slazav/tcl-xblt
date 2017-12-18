536b9c649d25d4f1bd7bab2b2a45bf198cfaa525
Vladislav Zavjalov <slazav@altlinux.org>
2017-12-18 17:09:31 +0200

    scroll.tcl: reset time scale if data range is changing (but scrollbar still 0..1)

--- tcl-xblt/scroll.tcl
+++ tcl-xblt/scroll.tcl
@@ -43,26 +43,29 @@ proc xblt::scroll::add {graph sbar args} {
 ## It moves scrollbar and runs user command (for data updates) if needed.
 proc xblt::scroll::sset {graph x1 x2} {
 
-  ## do only if range have been changed
-  if {$x1==$xblt::scroll::data($graph,oldx1) && $x2==$xblt::scroll::data($graph,oldx2)} return
-  set xblt::scroll::data($graph,oldx1) $x1
-  set xblt::scroll::data($graph,oldx2) $x2
-
-  $xblt::scroll::data($graph,scrollbar) set $x1 $x2
-
-  ## we no not run set_tscale and user command
-  ## too ofter
-  if {$xblt::scroll::data($graph,run_cmd_flag) > 0} return
-  incr xblt::scroll::data($graph,run_cmd_flag)
-
   ## limits in plot coordinates
   set lims [$graph axis limits x]
   set xmin [lindex $lims 0]
   set xmax [lindex $lims 1]
 
+  ## do only if range (in real ccordinates) have been changed
+  if {$xmin==$xblt::scroll::data($graph,oldx1) &&\
+      $xmax==$xblt::scroll::data($graph,oldx2)} return
+  set xblt::scroll::data($graph,oldx1) $xmin
+  set xblt::scroll::data($graph,oldx2) $xmax
+
+  $xblt::scroll::data($graph,scrollbar) set $x1 $x2
+
+  ## we no not run set_tscale and user command
+  ## too often
+  if {$xblt::scroll::data($graph,run_cmd_flag) > 0} return
+  incr xblt::scroll::data($graph,run_cmd_flag)
+
+
   # calculate size of plot area:
   set w [winfo width $graph]
   set w [expr {$w-140}];  # not accurate, we do not know size of legend + vertical axis
+
 
   ## format time axis if needed
   if {$xblt::scroll::data($graph,timefmt)} {
@@ -72,7 +75,6 @@ proc xblt::scroll::sset {graph x1 x2} {
   ## run user command if needed
   set cmd $xblt::scroll::data($graph,on_change)
   if {$cmd != ""} { eval $cmd $x1 $x2 $xmin $xmax $w }
-  update idletasks
   set xblt::scroll::data($graph,run_cmd_flag) [expr "$xblt::scroll::data($graph,run_cmd_flag) - 1"]
 }
 
