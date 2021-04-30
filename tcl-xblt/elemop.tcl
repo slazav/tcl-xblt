7d6c79e243853d6b75f7ba53f2113912a9082d1f
Vladislav Zavjalov <slazav@altlinux.org>
2021-04-30 11:37:08 +0100

    elemop.tcl: multiple blt::graph support

--- tcl-xblt/elemop.tcl
+++ tcl-xblt/elemop.tcl
@@ -11,53 +11,52 @@ proc xblt::elemop {graph args} {
 
 proc xblt::elemop::add {graph args} {
   variable data
-  bind data <ButtonPress-2>   "xblt::elemop::start $graph %x %y"
-  bind data <B2-Motion>       "xblt::elemop::do    $graph %x %y"
-  bind data <ButtonRelease-2> "xblt::elemop::end   $graph %x %y"
-  xblt::bindtag::add $graph data
-  set data(started) 0
+  bind $graph <ButtonPress-2>   "xblt::elemop::start $graph %x %y"
+  bind $graph <B2-Motion>       "xblt::elemop::do    $graph %x %y"
+  bind $graph <ButtonRelease-2> "xblt::elemop::end   $graph %x %y"
+  set data($graph,started) 0
 }
 
 ######################################################################
 proc xblt::elemop::start {graph xp yp} {
   variable data
   $graph element closest $xp $yp e -interpolate yes
-  set data(es) $e(name)
-  if {$data(es) == ""} return
+  set data($graph,es) $e(name)
+  if {$data($graph,es) == ""} return
   xblt::crosshairs::show $graph 0
-  set data(xpi) $xp
-  set data(yi) [$graph axis invtransform $data(es) $yp]
-  set data(sc) 1.0
-  set data(started) 1
+  set data($graph,xpi) $xp
+  set data($graph,yi) [$graph axis invtransform $data($graph,es) $yp]
+  set data($graph,sc) 1.0
+  set data($graph,started) 1
   return -code break
 }
 
 proc xblt::elemop::do {graph xp yp} {
   variable data
-  if {! $data(started)} return
-  set v $data(es)
-  set yi $data(yi)
+  if {! $data($graph,started)} return
+  set v $data($graph,es)
+  set yi $data($graph,yi)
   set y [$graph axis invtransform $v $yp]
   set y1 [lindex [$graph axis limits $v] 0]
   set y2 [lindex [$graph axis limits $v] 1]
-  set sc [expr {exp(($xp - $data(xpi)) / 100.0)}]
+  set sc [expr {exp(($xp - $data($graph,xpi)) / 100.0)}]
 
   if {[$graph axis cget $v -logscale]} {
-    set newmin [expr {exp(log($yi) - (log($y) - log($y1))*$data(sc)/$sc)}]
-    set newmax [expr {exp(log($yi) + (log($y2) - log($y))*$data(sc)/$sc)}]
+    set newmin [expr {exp(log($yi) - (log($y) - log($y1))*$data($graph,sc)/$sc)}]
+    set newmax [expr {exp(log($yi) + (log($y2) - log($y))*$data($graph,sc)/$sc)}]
   } else {
-    set newmin [expr {$yi - ($y - $y1)*$data(sc)/$sc}]
-    set newmax [expr {$yi + ($y2 - $y)*$data(sc)/$sc}]
+    set newmin [expr {$yi - ($y - $y1)*$data($graph,sc)/$sc}]
+    set newmax [expr {$yi + ($y2 - $y)*$data($graph,sc)/$sc}]
   }
   $graph axis configure $v -min $newmin -max $newmax
-  set data(sc) $sc
+  set data($graph,sc) $sc
   return -code break
 }
 
 proc xblt::elemop::end {graph xp yp} {
   variable data
-  if {! $data(started)} return
-  set data(started) 0
+  if {! $data($graph,started)} return
+  set data($graph,started) 0
   xblt::crosshairs::show $graph 1
   return -code break
 }
